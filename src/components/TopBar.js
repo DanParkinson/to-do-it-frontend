8f66e4053b17197d5d6766f320dbd78bf15c05c5
DanParkinson <danny.parkinson1996@gmail.com>
2025-02-18 11:36:31 +0000

    sidebar navigation set up for signout and design overhaul

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { Container, Row, Col, InputGroup, FormControl } from "react-bootstrap";
 import styles from "../styles/components/TopBar.module.css";
 import SearchBar from "./SearchBar";
 
 const TopBar = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <SearchBar placeholder="Search To-Do-It" />
-        </Col>
-      </Row>
-    </Container>
+    <header className={styles.TopBar}>
+      <SearchBar placeholder="Search To-Do-It" />
+    </header>
   );
 };
 
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,12 +1,30 @@
 import React from "react";
-import NavBar from "../components/NavBar";
+
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "../styles/layouts/MainLayout.module.css";
+
 import TopBar from "../components/TopBar";
+import SideBar from "../components/SideBar";
 
 const MainLayout = () => {
   return (
-    <div>
+    <>
       <TopBar />
-    </div>
+      <Container fluid className={styles.MainContainer}>
+        {/* Sidebar & Main Content */}
+        <Row className={styles.MainRow}>
+          {/* Sidebar (Navigation + Categories) */}
+          <Col lg={3} md={4} sm={2} xs={2} className={styles.SideBar}>
+            <SideBar />
+          </Col>
+
+          {/* Main Content */}
+          <Col lg={9} md={8} sm={10} xs={10} className={styles.MainContent}>
+            <h2> Main Content</h2>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
