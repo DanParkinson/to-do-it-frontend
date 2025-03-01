7b8f8264b69e11ae2a21a0bbfc1da8785e1d8729
DanParkinson <danny.parkinson1996@gmail.com>
2025-03-01 10:17:55 +0000

    Styles created for deletion modals

--- src/components/DeleteCategoryModal.js
+++ src/components/DeleteCategoryModal.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
+import styles from "../styles/components/ModalStyles.module.css";
 import btnStyles from "../styles/general/Button.module.css";
 
 const DeleteCategoryModal = ({ show, onClose, onConfirm, categoryName }) => {
   return (
-    <Modal show={show} onHide={onClose} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Delete Category</Modal.Title>
+    <Modal
+      show={show}
+      onHide={onClose}
+      centered
+      className={styles.ModalContainer}
+    >
+      <Modal.Header className={styles.ModalHeader}>
+        <Modal.Title className={styles.ModalTitle}>Delete Category</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className={styles.ModalBody}>
         <p>
           Are you sure you want to delete the category{" "}
           <strong>{categoryName}</strong>?
         </p>
-        <span>This will also delete all associated tasks.</span>
+        <p>This will also delete all associated tasks.</p>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={onClose}>
+      <Modal.Footer className={styles.ModalFooter}>
+        <Button className={btnStyles.PrimaryButton} onClick={onClose}>
           Cancel
         </Button>
-        <Button className={btnStyles.DeleteButton} onClick={() => onConfirm()}>
+        <Button className={btnStyles.PrimaryButton} onClick={() => onConfirm()}>
           Delete
         </Button>
       </Modal.Footer>
--- src/components/DeleteConfirmationModal.js
+++ src/components/DeleteConfirmationModal.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
+import styles from "../styles/components/ModalStyles.module.css";
 import btnStyles from "../styles/general/Button.module.css";
 
 const DeleteConfirmationModal = ({ show, onClose, onConfirm, taskTitle }) => {
   return (
-    <Modal show={show} onHide={onClose} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Confirm Deletion</Modal.Title>
+    <Modal
+      show={show}
+      onHide={onClose}
+      centered
+      className={styles.ModalContainer}
+    >
+      <Modal.Header className={styles.ModalHeader}>
+        <Modal.Title className={styles.ModalTitle}>
+          Confirm Deletion
+        </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className={styles.ModalBody}>
         <p>Are you sure you want to delete "{taskTitle}"?</p>
         <p>This action cannot be undone.</p>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={onClose}>
+      <Modal.Footer className={styles.ModalFooter}>
+        <Button className={btnStyles.PrimaryButton} onClick={onClose}>
           Cancel
         </Button>
-        <Button className={btnStyles.DeleteButton} onClick={onConfirm}>
+        <Button className={btnStyles.PrimaryButton} onClick={onConfirm}>
           Confirm Delete
         </Button>
       </Modal.Footer>
